1ec68b1629fe6e3adcc38284a56d69c8e2ab650f
MuTe43 <MuTe_wajdi@hotmail.fr>
2025-01-02 17:08:19 +0100

    Add initial game.py logic

---
+++ game.py
@@ -0,0 +1,25 @@
+import random
+
+with open("./words.txt") as wordfile:
+    words = wordfile.read().split()
+
+
+
+def isWordValid(choice):
+    while ("-" in choice or " " in choice):
+        choice = random.choice(words)
+
+
+
+def game():
+    numberTries = 6;
+    choice = random.choice(words)
+    isWordValid(choice)
+    for i in range(numberTries):
+        user = input("Guess a letter: ")
+        if (user == choice):
+            print("you win")
+            return
+        else:
+            print(f"wrong guess you have {numberTries - 1 - i} left ")
+game()
